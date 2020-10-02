5fffe70abaf33ef6444840def6f7c35facb79b68
clement barbaza <barbazaclement@gmail.om>
2020-10-02 18:42:47 +0200

    feat: init project with tailwind css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: LandingComponent },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
