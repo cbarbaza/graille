aa947eef653e55d23036f9ebb719069abe12b303
clement barbaza <barbazaclement@gmail.om>
2020-10-07 00:24:23 +0200

    fix: angular linter

--- src/app/data/info-card.interface.ts
+++ src/app/data/info-card.interface.ts
@@ -6,23 +6,23 @@ export interface IInfoCard {
 
 export const cardsInfo: IInfoCard[] = [
     {
-        "title": "que cuisines-t-on ?",
-        "text": 'Une cuisine <span class="font-semibold">végétarienne</span> qui permet d\'inclure tous nos bénéficiaires. A partir des invendus issus de producteurs locaux de la <span class="font-semibold">Région Auvergne Rhône Alpes</span>, d\'artisant boulangers et de commerçants militants du zéro déchet qu\'on transforme en savoureux repas complets. On transforme ainsi chaque Dimanche près de <span class="font-semibold">100-150</span> repas complets végétariens.'
+        title: 'que cuisines-t-on ?',
+        text: 'Une cuisine <span class="font-semibold">végétarienne</span> qui permet d\'inclure tous nos bénéficiaires. A partir des invendus issus de producteurs locaux de la <span class="font-semibold">Région Auvergne Rhône Alpes</span>, d\'artisant boulangers et de commerçants militants du zéro déchet qu\'on transforme en savoureux repas complets. On transforme ainsi chaque Dimanche près de <span class="font-semibold">100-150</span> repas complets végétariens.'
     },
     {
-        "title": "Ou cuisintes-ton ?",
-        "text": '<span class="font-semibold">En rue, en squat et en camps urbains</span> afin d\'aller à nos bénéficiaires et non l\'inverse. Notre accès aux suqat sont un gage de notre crédibilité et proximité avec nos bénéficiaires mais également une volonté de valoriser socialement cest lieux tout comme le sont les camps urbains'
+        title: 'Ou cuisintes-ton ?',
+        text: '<span class="font-semibold">En rue, en squat et en camps urbains</span> afin d\'aller à nos bénéficiaires et non l\'inverse. Notre accès aux suqat sont un gage de notre crédibilité et proximité avec nos bénéficiaires mais également une volonté de valoriser socialement cest lieux tout comme le sont les camps urbains'
     },
     {
-        "title": "Objectif zero dechet",
-        "text": 'Nous <span class="font-semibold">compostons</span> 90% de nos déchets qui sont organiques. Nos féculents légumineuses, huiles et épices sont issus du <span class="font-semibold">vrac</span>. Nous servons les repas dans <span class="font-semibold">des bocaux consignés</span>. Nos éponges sont réutilisable et en <span class="font-semibold">couture zéro déchet</span>. Nous contrôlons et optimisons notre <span class="font-semibold">consommation d\'eau.</span>'
+        title: 'Objectif zero dechet',
+        text: 'Nous <span class="font-semibold">compostons</span> 90% de nos déchets qui sont organiques. Nos féculents légumineuses, huiles et épices sont issus du <span class="font-semibold">vrac</span>. Nous servons les repas dans <span class="font-semibold">des bocaux consignés</span>. Nos éponges sont réutilisable et en <span class="font-semibold">couture zéro déchet</span>. Nous contrôlons et optimisons notre <span class="font-semibold">consommation d\'eau.</span>'
     },
     {
-        "title": "Militans",
-        "text": 'Nous combattons <span class="font-semibold">l\'anti-gaspillage alimentaire</span>. Pour celà nous pratiquons et initions au <span class="font-semibold">déchétarisme</span> puis responsabilisons toujours plus les commerçcants locaux en leur facilitant des solutions locales et durables. Les collectes partent en cuisine puis en <span class="font-semibold">Paniers du Mauvement #PourEux</span>'
+        title: 'Militans',
+        text: 'Nous combattons <span class="font-semibold">l\'anti-gaspillage alimentaire</span>. Pour celà nous pratiquons et initions au <span class="font-semibold">déchétarisme</span> puis responsabilisons toujours plus les commerçcants locaux en leur facilitant des solutions locales et durables. Les collectes partent en cuisine puis en <span class="font-semibold">Paniers du Mauvement #PourEux</span>'
     },
     {
-        "title": "Pas de planète B",
-        "text": 'On collecte et livre les invendus à <span class="font-semibold">90% à vélo</span>. Equipés de triporteurs, biporteurs mais également du soutiens des cyclistes du Mouvement #PourEux. Celà à la facultée de pourvoir servir des bénéficiaires <span class="font-semibold">plus isolés</span> ou des personnes <span class="font-semibold">mal-logès</span> en situation de forte précarité. Objectif 100% à vélo.'
+        title: 'Pas de planète B',
+        text: 'On collecte et livre les invendus à <span class="font-semibold">90% à vélo</span>. Equipés de triporteurs, biporteurs mais également du soutiens des cyclistes du Mouvement #PourEux. Celà à la facultée de pourvoir servir des bénéficiaires <span class="font-semibold">plus isolés</span> ou des personnes <span class="font-semibold">mal-logès</span> en situation de forte précarité. Objectif 100% à vélo.'
     }
 ];
