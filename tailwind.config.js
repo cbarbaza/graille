5d6fe293233c48c4929fb3c12dc35ed47cb6cfbc
Marc-Olivier Castagnetti <marcolivier.castagnetti@gmail.com>
2020-10-17 18:36:23 +0200

    Use tailwind config to adjust colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   },
   purge: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: ['Fira Sans', 'system-ui', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'Roboto', 'sans-serif'],
+      sans: ['Roboto', 'system-ui', 'BlinkMacSystemFont', '-apple-system', 'Segoe UI', 'sans-serif'],
+    },
+    fontSize: {
+      10: '1rem',
+      12: '1.2rem',
+      14: '1.4rem',
+      16: '1.6rem',
+      18: '1.8rem',
+      22: '2.2rem',
+      28: '2.8rem',
+    },
+    extend: {
+      colors: {
+        primary: '#c29a6a',
+        secondary: '#4a5568',
+      },
+    },
   },
   variants: {},
   plugins: [],
